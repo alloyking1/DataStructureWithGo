1d7bbb91115cc46e3663689ff8681975b8a2f21e
alloyking1 <alloyking1@gmail.com>
2021-09-04 09:33:19 -0400

    implemented the Extraction process from the heap

--- heaps/heap.go
+++ heaps/heap.go
@@ -24,6 +24,51 @@ func (h *MaxHeap) maxHeapifyUp(index int) {
 	}
 }
 
+// extract, return the largest key and remove it from the heap
+func (h *MaxHeap) Extract() int {
+	extracted := h.array[0]
+
+	if len(h.array) == 0 {
+		fmt.Println("Cant extract because the length of the array is 0")
+		return -1
+	}
+
+	l := len(h.array)-1
+	h.array[0] =h.array[l]
+	h.array = h.array[:l]
+
+	h.maxHeapifyDown(0)
+
+	return extracted
+}
+
+func (h *MaxHeap) maxHeapifyDown(index int) {
+	lastIndex := len(h.array)-1
+	l,r := left(index), right(index)
+	childToCompare := 0
+
+	for l <= lastIndex {
+		// when left child is only child
+		if l == lastIndex {
+			childToCompare = l
+		}else if h.array[l] > h.array[r] {
+			childToCompare = l //when left child is larger
+		} else {
+			childToCompare = r //when right child is larger
+		}
+		
+		// compare array value and swap
+		if h.array[index] < h.array[childToCompare] {
+			h.swap(index, childToCompare)
+			index = childToCompare
+			l, r = left(index), right(index)
+		}else{
+			return
+		}
+		
+	}
+}
+
 // we need to get the parent
 func parent (i int) int {
 	return (i-1)/2
@@ -50,7 +95,7 @@ func main (){
 	fmt.Println(m)
 
 	// create a heap
-	buildHeap := []int{10, 20, 30, 15, 50, 33,34}
+	buildHeap := []int{10, 20, 30, 5, 7, 9, 11, 13, 15, 17}
 
 	// insert the heap into struct
 	for _, v := range buildHeap {
@@ -58,4 +103,9 @@ func main (){
 		fmt.Println(m)
 	}
 	//sort heap
+
+	for i := 0; i <= 5; i++ {
+		m.Extract()
+		fmt.Println(m)
+	}
 }
